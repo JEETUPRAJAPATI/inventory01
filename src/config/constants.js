f3e7446d5e02713632d128fbaa1d4a4bd09e6e11
jeetu-mj <jeetu.mj123@gmail.com>
2025-03-24 14:32:05 +0530

    Update constact url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://inventorybackend-dcyp.onrender.com';
+export const API_BASE_URL = 'https://inventorybackend-dcyp.onrender.com/api';
 
-// live Url https://inventorybackend-dcyp.onrender.com
+// live Url https://inventory-zmsp.onrender.com
 // local http://localhost:3010
